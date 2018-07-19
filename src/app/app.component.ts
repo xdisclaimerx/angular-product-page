81716c41890d8ba1aa5ce2e689e280effac373fb
Christian R. Amaro <xtian.amaro@gmail.com>
2018-07-19 17:38:13 +0800

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,27 @@ import { Component } from '@angular/core';
   <div class="container">
     <app-nav></app-nav>
     <router-outlet></router-outlet>
+    <div class="jumbotron">
+      <h1 class="display-3" innerHTML="{{contents}}"></h1>
+      <hr class="my-4">
+    </div>
   </div>
-  `
+  `,
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  mainview:any;
+  contents:any;
+  	constructor(public service: DataService) { 
+
+	}
+
+  ngOnInit() {
+    this.service.getMainView()
+		.then(data => {
+
+			this.mainview = data;
+      this.contents = this.mainview.content;
+      console.log(data)
+    });
+  }
 }
